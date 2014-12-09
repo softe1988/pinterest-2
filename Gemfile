76a47b07e94fca7629e78e099d4bb8259ec7f9b9
Simone Battiste-Alleyne <softe1988@gmail.com>
2014-12-09 09:12:33 -0500

    Added authentication and pin index styling

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'devise'
 # allows us to upload files
 gem 'carrierwave'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :boards
 	has_many :pins
 
-	validates :first_name,  :last_name, :email, presence: true 
+	# validates :first_name,  :last_name, :email, presence: true 
 	
 	#creating new model so we can call @user.full_name 
 	def full_name
